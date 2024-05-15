e7bd41a74d9c7742372f78911a12508f9bdf8a5e
Mozammal Hossain <144788896+Mozammal-hossain@users.noreply.github.com>
2024-05-15 20:28:15 +0600

    feat: Added TextField and Button

--- app/src/main/java/com/example/authmodulejpc/MainActivity.kt
+++ app/src/main/java/com/example/authmodulejpc/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.authmodulejpc
 
 import android.os.Bundle
+import android.text.Layout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
@@ -16,6 +17,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
@@ -33,13 +35,21 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Canvas
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Outline
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.room.util.copy
 import com.example.authmodulejpc.ui.theme.AuthModuleJPCTheme
 
 class MainActivity : ComponentActivity() {
@@ -53,63 +63,109 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = Color.White,
                 ) {
-                    Column {
-                        Spacer(modifier = Modifier.size(50.dp))
+                    Column (
+                       verticalArrangement = Arrangement.SpaceBetween,
+                        modifier = Modifier.padding(24.dp),
+                    ){
+                        Spacer(modifier = Modifier.size(101.dp))
                         PageName(
                             pageTitle = "Log in to Authy",
                             pageSubTitle = "Welcome back! Sign in using your social account or email to continue us",
                         )
-                        InputFieldWithLabel(
-                            label = "Email",
-                            hintText = "Enter your email",
-                            keyboard = KeyboardType.Email,
-                        )
-                        InputFieldWithLabel(
-                            label = "Password",
-                            hintText = "Enter your password",
-                            keyboard = KeyboardType.Password,
-                        )
-
-                        Box(modifier = Modifier.size(10.dp))
-
-                        Row (
+                        Column(
                             modifier = Modifier
-                                .fillMaxWidth(),
-                            horizontalArrangement = Arrangement.SpaceBetween,
-                        )
-                        {
-                            Row(
-                                verticalAlignment = Alignment.CenterVertically,
-                            ) {
-                                Button(
-                                    onClick = {
-                                        checked = !checked
-                                    },
-                                    modifier = Modifier.size(16.dp),
-                                    shape = RoundedCornerShape(5.dp),
-                                    border = BorderStroke(width = 2.dp,
-                                        color =  MaterialTheme.colorScheme.primary,),
-                                    colors = ButtonDefaults.buttonColors(containerColor = Color.White),
+                                .fillMaxWidth()
+                        ) {
+                            InputFieldWithLabel(
+                                label = "Email",
+                                hintText = "Enter your email",
+                                keyboard = KeyboardType.Email,
+                            )
+                            Box(modifier = Modifier.size(30.dp))
 
-                                ) {}
-                                Box(modifier = Modifier.size(7.dp))
+
+                            InputFieldWithLabel(
+                                label = "Password",
+                                hintText = "Enter your password",
+                                keyboard = KeyboardType.Password,
+                            )
+
+                            Box(modifier = Modifier.size(10.dp))
+
+                            Row (
+                                modifier = Modifier
+                                    .fillMaxWidth(),
+                                horizontalArrangement = Arrangement.SpaceBetween,
+                            )
+                            {
+                                Row(
+                                    verticalAlignment = Alignment.CenterVertically,
+                                ) {
+                                    Button(
+                                        onClick = {
+                                            checked = !checked
+                                        },
+                                        modifier = Modifier.size(16.dp),
+                                        shape = RoundedCornerShape(5.dp),
+                                        border = BorderStroke(width = 2.dp,
+                                            color =  Color(0xFF24786D),),
+                                        colors = ButtonDefaults.buttonColors(containerColor = Color.Gray.copy(alpha = 0.1f)),
+
+                                        ) {}
+                                    Box(modifier = Modifier.size(7.dp))
+                                    Text(
+                                        text = "Remember Me",
+                                        fontWeight = FontWeight.Medium,
+                                        fontSize = 14.sp,
+                                        color = Color(0xFF24786D),
+                                    )
+                                }
                                 Text(
-                                    text = "Remember Me",
+                                    text = "Forgot Password?",
                                     fontWeight = FontWeight.Medium,
                                     fontSize = 14.sp,
-                                    color = MaterialTheme.colorScheme.primary,
+                                    color = Color(0xFF24786D),
                                 )
                             }
+
+                        }
+                        Column {
+                            Button(
+                                onClick = { checked = !checked },
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .height(60.dp),
+                                shape = RoundedCornerShape(
+                                    topStart = 16.dp,
+                                    topEnd = 16.dp,
+                                    bottomStart = 16.dp,
+                                    bottomEnd = 16.dp
+                                ),
+                                colors = ButtonDefaults.buttonColors(
+                                    containerColor = Color(0xFF24786D)
+                                ),
+                            ) {
+                                Text(
+                                    text = "Login",
+                                )
+                            }
+
+                            Spacer(Modifier.height(20.dp))
+
                             Text(
-                                text = "Forgot Password?",
+                                text = "Don’t have any account? Sign Up",
                                 fontWeight = FontWeight.Medium,
                                 fontSize = 14.sp,
-                                color = MaterialTheme.colorScheme.primary,
+                                color = Color(0xFF24786D),
+                                modifier = Modifier
+                                    .align(alignment = Alignment.CenterHorizontally)
                             )
+
+                            Spacer(Modifier.height(37.dp))
                         }
-
-
                     }
+
+
                 }
             }
         }
@@ -131,7 +187,7 @@ fun PageName(
         )
         Text(
             text = pageSubTitle,
-            color = MaterialTheme.colorScheme.secondary,
+            color = Color.Gray.copy(alpha = 0.8f),
             textAlign = TextAlign.Center,
             modifier =
             Modifier.padding(
@@ -160,10 +216,8 @@ fun InputFieldWithLabel(
             text = label,
             fontWeight = FontWeight.Medium,
             fontSize = 14.sp,
-            color = MaterialTheme.colorScheme.primary
+            color = Color(0xFF24786D)
         )
-
-        Spacer(modifier = Modifier.size(15.dp))
 
         BasicTextField(
             value = textFieldValue,
@@ -174,16 +228,39 @@ fun InputFieldWithLabel(
                     modifier = Modifier
                         .fillMaxWidth()
                         .background(Color.White)
-                        .padding(0.dp)
+                        .height(60.dp)
+                        .drawBehind {
+                            drawLine(
+                                color = Color.Gray.copy(alpha = 0.5f),
+                                start = Offset(0f, size.height),
+                                end = Offset(size.width, size.height),
+                            )
+                        }
+                    ,
                 ) {
                     if (textFieldValue.text.isEmpty()) {
                         Text(
                             text = hintText,
-                            color = MaterialTheme.colorScheme.primary.copy(alpha = 0.2f),
-                            fontSize = MaterialTheme.typography.bodyMedium.fontSize // Adjust font size as needed
+                            color = Color(0xFF24786D).copy(alpha = 0.2f),
+                            fontSize = 15.sp,
+                            fontWeight = FontWeight.Medium,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(
+                                    vertical = 15.dp,
+                                ),
+
                         )
                     }
-                    innerTextField()
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(
+                                vertical = 15.dp,
+                            ),
+                    ) {
+                        innerTextField()
+                    }
                 }
             },
 
