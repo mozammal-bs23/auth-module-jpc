18f9c259b5fc7308685f256565ce917385c103d3
Mozammal Hossain <144788896+Mozammal-hossain@users.noreply.github.com>
2024-05-15 15:46:41 +0600

    initial commit

---
+++ app/src/main/java/com/example/authmodulejpc/MainActivity.kt
@@ -0,0 +1,186 @@
+package com.example.authmodulejpc
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.authmodulejpc.ui.theme.AuthModuleJPCTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            var checked by remember { mutableStateOf(false) }
+
+            AuthModuleJPCTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = Color.White,
+                ) {
+                    Column {
+                        Spacer(modifier = Modifier.size(50.dp))
+                        PageName(
+                            pageTitle = "Log in to Authy",
+                            pageSubTitle = "Welcome back! Sign in using your social account or email to continue us",
+                        )
+                        InputFieldWithLabel(
+                            label = "Email",
+                            hintText = "Enter your email",
+                            keyboard = KeyboardType.Email,
+                        )
+                        InputFieldWithLabel(
+                            label = "Password",
+                            hintText = "Enter your password",
+                            keyboard = KeyboardType.Password,
+                        )
+
+                        Box(modifier = Modifier.size(10.dp))
+
+                        Row (
+                            modifier = Modifier
+                                .fillMaxWidth(),
+                            horizontalArrangement = Arrangement.SpaceBetween,
+                        )
+                        {
+                            Row(
+                                verticalAlignment = Alignment.CenterVertically,
+                            ) {
+                                Button(
+                                    onClick = {
+                                        checked = !checked
+                                    },
+                                    modifier = Modifier.size(16.dp),
+                                    shape = RoundedCornerShape(5.dp),
+                                    border = BorderStroke(width = 2.dp,
+                                        color =  MaterialTheme.colorScheme.primary,),
+                                    colors = ButtonDefaults.buttonColors(containerColor = Color.White),
+
+                                ) {}
+                                Box(modifier = Modifier.size(7.dp))
+                                Text(
+                                    text = "Remember Me",
+                                    fontWeight = FontWeight.Medium,
+                                    fontSize = 14.sp,
+                                    color = MaterialTheme.colorScheme.primary,
+                                )
+                            }
+                            Text(
+                                text = "Forgot Password?",
+                                fontWeight = FontWeight.Medium,
+                                fontSize = 14.sp,
+                                color = MaterialTheme.colorScheme.primary,
+                            )
+                        }
+
+
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun PageName(
+    pageTitle: String,
+    pageSubTitle: String,
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(
+            text = pageTitle,
+            fontWeight = FontWeight.Bold,
+            fontSize = 18.sp,
+        )
+        Text(
+            text = pageSubTitle,
+            color = MaterialTheme.colorScheme.secondary,
+            textAlign = TextAlign.Center,
+            modifier =
+            Modifier.padding(
+                horizontal = 41.dp,
+                vertical = 8.dp,
+            ),
+        )
+    }
+}
+
+@Composable
+fun InputFieldWithLabel(
+    label: String,
+    hintText: String,
+    keyboard: KeyboardType,
+) {
+    var textFieldValue by remember { mutableStateOf(TextFieldValue(text = "")) }
+
+    Column(
+        modifier = Modifier
+            .padding(0.dp)
+    ) {
+        Text(
+            text = label,
+            modifier = Modifier.padding(bottom = 4.dp),
+            fontWeight = FontWeight.Medium,
+            fontSize = 14.sp,
+            color = MaterialTheme.colorScheme.primary,
+        )
+        TextField(
+            value = textFieldValue,
+            onValueChange = {
+                textFieldValue = it
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = keyboard),
+            modifier =
+            Modifier
+                .fillMaxWidth()
+                .background(Color.White)
+                .padding(0.dp),
+            singleLine = true,
+            colors =
+            TextFieldDefaults.colors(
+                unfocusedContainerColor = Color.White,
+                focusedContainerColor = Color.White,
+            ),
+            placeholder = {
+                Text(
+                    text = hintText,
+                    color = MaterialTheme.colorScheme.primary.copy(alpha = 0.2f),
+                )
+            },
+        )
+    }
+}
---
+++ app/src/main/java/com/example/authmodulejpc/ui/theme/Color.kt
@@ -0,0 +1,22 @@
+package com.example.authmodulejpc.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Primary = Color(0xFF24786D)
+val Secondary = Color(0xFFFFFFFF)
+val Tertiary = Color(0xFF03DAC5)
+val Error = Color(0xFFF04A4C)
+
+val DarkPrimary = Color(0xFF24786D)
+val DarkSecondary = Color(0xFF757575)
+val DarkTertiary = Color(0xFF03DAC5)
+val DarkError = Color(0xFFE69FA0)
+
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
